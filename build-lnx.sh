a77ace877c3229df513ac2d2c39c4a593343ef30
funbiscuit <kokurinsv@gmail.com>
2020-04-10 23:36:19 +0300

    Add sample files for build on linux and windows

---
+++ build-lnx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p cmake-build-release
+cd cmake-build-release || exit
+cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" ..
+cmake --build . --target spacedisplay -- -j 3
+cd ..
+mkdir -p bin
+cp cmake-build-release/spacedisplay bin/spacedisplay
---
+++ build-win-msvc15_x64.bat
@@ -0,0 +1,16 @@
+@echo off
+if not exist "cmake_win_path.txt" (
+echo Rename cmake_win_path.example.txt to cmake_win_path.txt and provide path to cmake.exe
+exit
+)
+
+set /p cmake_path=<cmake_win_path.txt
+
+if not exist "cmake-build-msvc15_x64-release\" mkdir cmake-build-msvc15_x64-release
+if not exist "bin\" mkdir bin
+
+cd cmake-build-msvc15_x64-release
+%cmake_path% -G "Visual Studio 15 2017 Win64" ..
+%cmake_path% --build . --config Release --target spacedisplay --
+copy Release\spacedisplay.exe ..\bin\spacedisplay.exe
+cd ..
