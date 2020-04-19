9fdf6418c851df169da1024bdf74ba4799776c3d
funbiscuit <kokurinsv@gmail.com>
2020-04-19 13:03:53 +0300

    Move application code to separate static library

---
+++ lib/mainapp.cpp
@@ -0,0 +1,38 @@
+
+#include "mainapp.h"
+
+#include <QApplication>
+
+#include "mainwindow.h"
+#include "resources.h"
+
+
+int MainApp::run(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("com.github.funbiscuit");
+    QCoreApplication::setApplicationName("Space Display");
+    QCoreApplication::setApplicationVersion("1.0.0");
+//    QCommandLineParser parser;
+//    parser.setApplicationDescription(QCoreApplication::applicationName());
+//    parser.addHelpOption();
+//    parser.addVersionOption();
+//    parser.addPositionalArgument("file", "The file to open.");
+//    parser.process(app);
+
+
+    using namespace ResourceBuilder;
+    auto& r = Resources::get();
+
+    QIcon appIcon(r.get_vector_pixmap(ResId::__ICONS_SVG_APPICON_SVG, 16));
+    //add other sizes
+    for(int sz=32;sz<=256;sz*=2)
+        appIcon.addPixmap(r.get_vector_pixmap(ResId::__ICONS_SVG_APPICON_SVG, sz));
+
+    QApplication::setWindowIcon(appIcon);
+
+    MainWindow mainWin;
+    mainWin.show();
+    return QApplication::exec();
+}
+
---
+++ lib/mainapp.h
@@ -0,0 +1,10 @@
+#ifndef SPACEDISPLAY_MAINAPP_H
+#define SPACEDISPLAY_MAINAPP_H
+
+class MainApp
+{
+public:
+    int run(int argc, char *argv[]);
+};
+
+#endif //SPACEDISPLAY_MAINAPP_H
