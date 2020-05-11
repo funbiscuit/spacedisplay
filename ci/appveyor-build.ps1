64ff363221475a179eb70b37fc44b477562f8b52
funbiscuit <kokurinsv@gmail.com>
2020-05-11 15:31:50 +0300

    Increment version, remove win builds with cmd

--- ci/appveyor-build.ps1
+++ ci/appveyor-build.ps1
@@ -8,9 +8,6 @@ $sufx = @("msvc15_x64", "msvc15_x86")
 $qt_dlls = @("Qt5Core.dll","Qt5Gui.dll","Qt5Svg.dll","Qt5Widgets.dll")
 $qt_plug = @("platforms\qwindows.dll")
 
-$cmd_sfx = @("", "-cmd") #empty for normal version
-$cmd_var = @("Off", "On")
-
 for($i = 0; $i -lt $vc_arch.length; $i++){
     if($i -eq 0){
        $qt_path = "$env:QT_DIR_WIN_64"
@@ -21,29 +18,25 @@ for($i = 0; $i -lt $vc_arch.length; $i++){
     $vc = $sufx[$i]
     $vc_gen_sfx = $vc_arch[$i]
 
-    for($k = 0; $k -lt $cmd_var.length; $k++) {
-        $cs = $cmd_sfx[$k]
-        $cv = $cmd_var[$k]
-        # create build dir if it was not restored from cache
-        if(!(test-path "build$cs-$vc")){ mkdir "build$cs-$vc" }
-        Set-Location "build$cs-$vc"
-        cmake -DQT_WIN_PATH="$qt_path" -DWIN32_CONSOLE="$cv" -G "Visual Studio 15 2017$vc_gen_sfx" ..
-        cmake --build . --config Release
-        Set-Location ..
+    # create build dir if it was not restored from cache
+    if(!(test-path "build-$vc")){ mkdir "build-$vc" }
+    Set-Location "build-$vc"
+    cmake -DQT_WIN_PATH="$qt_path" -DWIN32_CONSOLE="Off" -G "Visual Studio 15 2017$vc_gen_sfx" ..
+    cmake --build . --config Release
+    Set-Location ..
 
-        # create folders for binaries
-        mkdir "bin$cs-$vc"
-        mkdir "bin$cs-$vc\platforms"
+    # create folders for binaries
+    mkdir "bin-$vc"
+    mkdir "bin-$vc\platforms"
 
-        # copy executable and dll's
-        Move-Item "build$cs-$vc\app-gui\Release\spacedisplay_gui.exe" "bin$cs-$vc\spacedisplay.exe"
-        for($j = 0; $j -lt $qt_dlls.length; $j++){
-            $dll = $qt_dlls[$j]
-            Copy-Item "$qt_path\bin\$dll" "bin$cs-$vc\$dll"
-        }
-        for($j = 0; $j -lt $qt_plug.length; $j++){
-            $dll = $qt_plug[$j]
-            Copy-Item "$qt_path\plugins\$dll" "bin$cs-$vc\$dll"
-        }
+    # copy executable and dll's
+    Move-Item "build-$vc\app-gui\Release\spacedisplay_gui.exe" "bin-$vc\spacedisplay.exe"
+    for($j = 0; $j -lt $qt_dlls.length; $j++){
+        $dll = $qt_dlls[$j]
+        Copy-Item "$qt_path\bin\$dll" "bin-$vc\$dll"
+    }
+    for($j = 0; $j -lt $qt_plug.length; $j++){
+        $dll = $qt_plug[$j]
+        Copy-Item "$qt_path\plugins\$dll" "bin-$vc\$dll"
     }
 }
